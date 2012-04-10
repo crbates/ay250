30d8deedb7e776e76695a0a665082c83fd5b865e
Cameron Bates <cameron.r.bates@gmail.com>
2012-04-10 10:24:29 -0700

    add inline documentation, readme, and performance plot

--- hw9/hw9.py
+++ hw9/hw9.py
@@ -2,12 +2,19 @@
 Homework 9
 Author: Cameron Bates
 
+This script compares three different methods of computing pi. One method is
+serial, one method uses multiprocessing, and one uses ipythons ipcluster 
+method. Prior to running this an ipcluster must be started.  
 '''
 
 from time import time
 from multiprocessing import cpu_count
 
 def serial(number_of_darts):
+    '''
+    This function runs the basic serial dart throwing for the computation of
+    pi for the input number of dart throws.
+    '''
     from random import uniform
     from math import sqrt
     number_of_darts_in_circle = 0
@@ -18,12 +25,15 @@ def serial(number_of_darts):
     return number_of_darts_in_circle
 
 def serialpi(number_of_darts):
+    #time the serial method
     start = time()
     serial(number_of_darts)
     stop = time()
     return stop-start
 
 def calcdartspp(nproc,ndarts):
+    #this is a helper method to split the darts per processor evenly while
+    #making sure that the total number of darts computed is correct.
     dpp = []
     mod = ndarts % nproc
     rough = ndarts/nproc
@@ -33,40 +43,61 @@ def calcdartspp(nproc,ndarts):
     dpp[-1] += mod
     return dpp  
 
-def multiprocessingpi(number_of_darts):    
+def multiprocessingpi(number_of_darts):
+    #This function uses the multiprocessing module to parallelize the
+    #serial computation    
     from multiprocessing import Pool  
     final = 0
+    #get number of cores
     nproc = cpu_count()
     p = Pool(nproc)
+    #start timing
     start = time()
     dartspp = calcdartspp(nproc,number_of_darts)
+    #Use the map_async method to split the computation up
     result = p.map_async(serial,dartspp)
     result.wait()
     poolresult = result.get()
+    #sum the final result
     for item in poolresult:
         final += item
     stop = time()
+    #we don't use the final result we just care about the time
     
     #print 4*final/float(number_of_darts) 
     return stop-start
 
 def ipcluster(number_of_darts):
+    '''
+    This method uses an ipython ipcluster to parallelize the serial
+    computation of pi.
+    '''
+    #create a client
     from IPython.parallel import Client
     c = Client() 
     final = 0
+    #determine the number of engines running
     nproc = len(c.ids)
+    #start timing
     start = time()
     dartspp = calcdartspp(nproc,number_of_darts)
-    #print dartspp
+    #use the map_sync method to split the computation up
     result = c[:].map_sync(serial,dartspp)
     for item in result:
         final += item
     stop = time()
+    
     return stop-start
 
 if __name__ == '__main__':
+    '''
+    This main function runs the three different methods for various total 
+    numbers of darts.
+    '''
     import numpy as np
+    #sizes to use
     n = [25,50,300,1000,5000,20000,100000,1000000]#np.logspace(2,7,8)
+    #initialize result arrays
     mp = np.zeros(8)
     serialr = np.zeros(8)
     ipy = np.zeros(8)
@@ -78,10 +109,12 @@ if __name__ == '__main__':
     for index, item in enumerate(n):
         serialr[index] = serialpi(item)
     import matplotlib.pyplot as plt
+    #calculate the darts per second for each method
     dpsmp = np.array(n)/mp
     dpsipy = np.array(n)/ipy
     dpsserialr = np.array(n)/serialr
     
+    #plot the execution time as a function of the number of darts thrown
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
     plt.plot(n,mp,'o-',label='multiprocessing')
@@ -92,12 +125,14 @@ if __name__ == '__main__':
     plt.ylabel('Execution Time (Seconds), solid line')
     plt.legend(loc=2)
     
+    #add a second axis with the darts per second
     ax2 = ax1.twinx()
     ax2.plot(n,dpsmp,'o--')
     ax2.hold(True)
     plt.ylabel('Simulation Rate (Darts/Second), dashed line')
     ax2.plot(n,dpsipy,'o--')
     ax2.plot(n,dpsserialr,'o--')
+    #adjust the upper x limit so the final point is visible
     ax = plt.axis()
     nx = []
     nx.append(ax[0])
@@ -105,6 +140,7 @@ if __name__ == '__main__':
     nx.append(ax[2])
     nx.append(ax[3])
     plt.axis(nx)
+    #make the right axis label visible
     fig.subplots_adjust(right=0.85)
     plt.show()
     
---
+++ hw9/readme.md
@@ -0,0 +1,10 @@
+#Homework 9 AY 250 Sprign 2012  
+Author: Cameron Bates
+
+Prior to running the hw9.py script start an ipython cluster with the desired
+number of engines. This can be done using the command:  
+ipcluster start -n 4  
+This starts 4 engines. The output of running the script on a 2011 Macbook air
+with a 1.8GHz dual-core processor with hyperthreading(4 effective threads) is
+included in performanceplot.pdf. Running the hw9.py will produce the same 
+output figure.
